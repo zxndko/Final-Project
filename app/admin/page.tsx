e5f30b939ab73e767bf07de8951a6863162b6459
SEE <en.cpnt@gmail.com>
2026-02-10 13:19:43 +0700

    Add new files

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,14 @@
 // app/admin/page.tsx
 import Link from 'next/link';
 import HideHeader from '@/components/HideHeader';
+import HideFooter from '@/components/HideFooter';
 
 export default function AdminPanelPage() {
     return (
         <section className="admin-dashboard">
-            {/* client component hides global header when this page mounts */}
             <HideHeader />
+            <HideFooter />
+
             <div className="container">
                 <h1>ยินดีต้อนรับสู่ Admin Panel</h1>
                 <p>เข้าสู่ระบบด้วยสิทธิ์ผู้ดูแลระบบเรียบร้อยแล้ว</p>
@@ -16,10 +18,12 @@ export default function AdminPanelPage() {
                         <h3>จัดการนัดหมาย</h3>
                         <p>ดูและอนุมัติการจองนัดหมาย</p>
                     </Link>
-                      <Link href="/admin/docters" className="action-card">
+
+                    <Link href="/admin/docters" className="action-card">
                         <h3>จัดการบัญชีผู้ใช้</h3>
                         <p>เพิ่ม แก้ไข หรือลบข้อมูลหมอ</p>
                     </Link>
+
                     <div className="action-card">
                         <h3>จัดการหน้าเว็บไซต์</h3>
                         <p>เพิ่ม แก้ไข ลบ หรืออัพเดทข้อมูล</p>
